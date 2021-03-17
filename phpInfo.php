8ab85033e43227a67cfd556fa021d9356021cf5a
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-03-17 20:13:56 +0100

    echo, var_dump, strtolower examples created

--- phpInfo.php
+++ phpInfo.php
@@ -1,1 +1,15 @@
-<?php phpinfo(); ?>
+<?php
+    //php phpinfo();
+
+    $doorIsOpen = true;
+    $windowIsOpen = true;
+
+    $s = 12;
+    echo $s . " ";
+    var_dump(doorIsOpen && windowIsOpen);
+
+    $stringLower = 'Hello, World';
+    strtolower(stringLower);
+    echo lowerString;
+
+
