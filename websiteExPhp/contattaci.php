da86bb598f24e9f5459fceee7e91ce9c65f7bf04
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-04 21:07:25 +0200

    condiction added with userIsAuthenticated function

--- websiteExPhp/contattaci.php
+++ websiteExPhp/contattaci.php
@@ -1,3 +1,9 @@
+<?php require_once("common.php"); ?>
+<?php if(userIsAuthenticated() !== true) {
+    header("location: login.php");
+}
+?>
+
 <html>
     <body>
         <?php require_once 'menu.php';?>
