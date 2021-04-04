f7dac134f02197c172b631d4e4dc181429427ae8
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-04 21:11:25 +0200

    existUser changed with new var,loadUser & login fun moved in common php

--- websiteExPhp/login.php
+++ websiteExPhp/login.php
@@ -1,45 +1,17 @@
+<?php require_once("common.php"); ?>
+
 <?php
 
-session_start();
-
-// restituisce l array di tutti gli utenti leggendo il file csv
-function loadUsers():array {
-  if (($handle = fopen("users.csv", "r")) !== FALSE) {
-    $users = [];
-    while (($data = fgetcsv($handle)) !== FALSE) {
-      $users[] = $data;
-    }
-    fclose($handle);
-    return $users;
-  }
-}
-
-
-// controlla se l user esiste ritornando true
-function existsUser($username,$password):bool
-{
-  $users = loadUsers();
-  foreach ($users as $user) {
-    $currentUser = $user[0];
-    $currentPassword = $user[1];
-
-    if($username === $currentUser && $password === $currentPassword) {
-      return true;
-    }
-  }
-  return false;
-}
 
 // instanzio le credenziali degli utenti, username e password, e salvo in una variabile di sessione l username, per utilizzarlo nel mio HTML
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $existsUser = existsUser($username, $password);
+    $loginSuccessful = login($username, $password);
 
-    if($existsUser === true) {
+    if($loginSuccessful === true) {
       $_SESSION["username"] = $username;
     }
-    var_dump($existsUser);
 }
 ?>
 
@@ -50,10 +22,11 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 
         <div class="container">
 
-          <?php echo $_SESSION["username"];
-            echo " ha fatto l`accesso"
-            ?>
           <form action="login.php" method="POST">
+              <?php if (userIsAuthenticated() === true):?>
+                <p>Sei gia loggato!</p>
+              <?php endif; ?>
+
             <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Username</label>
               <input type="username" name="username" class="form-control" id="exampleInputUsername">
