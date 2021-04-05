60f0764983ac52e6b2fed7f9843f0e5f9015d52f
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-05 22:32:20 +0200

    require_once navbar.php, mail fun created, new call by printLog()

--- websiteExPhp/contattaci.php
+++ websiteExPhp/contattaci.php
@@ -1,12 +1,51 @@
 <?php require_once("common.php"); ?>
+
 <?php if(userIsAuthenticated() !== true) {
     header("location: login.php");
 }
 ?>
 
+<?php if($_SERVER["REQUEST_METHOD"] === "POST") {
+  $message = $_POST["message"];
+  if(!empty($message)) {
+    $emailIsSent = mail('angeloalbertomnt@gmail.com', 'saluto', sprintf('qualcuno di ha scritto: "%s"',$message));
+    printLog(sprintf("L`utente %s ha inviato un email"), $_SESSION["username"]);
+
+  }
+}
+
+?>
 <html>
+    <?php require_once('bootstrap_css_link.php')?>
     <body>
-        <?php require_once 'menu.php';?>
+        <?php require_once 'navbar.php';?>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <!-- la condizione controlla se l email esiste dando true, e se e` stata inviata  -->
+              <?php if (isset($emailIsSent) && $emailIsSent === true): ?>
+
+                Messaggio inviato <a href="contattaci.php">Invia un altro messaggio</a>
+
+              <?php else: ?>
+
+                <form action="contattaci.php" method="POST">
+
+                  <p>Contattaci con questo form!</p>
+
+                  <div class="mb-3">
+                    <label class="form-label">Message</label>
+                    <textarea name="message" class="form-control" id="exampleInputMessage"></textarea>
+                  </div>
+
+                  <button type="submit" class="btn btn-primary">INVIA</button>
+                </form>
+
+              <?php endif; ?>
+
+            </div>
+          </div>
+        </div>
     </body>
-    <p>CONTATTACI</p>
 </html>
