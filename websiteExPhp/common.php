006880889a731f8b3f075f10714528d6140d5bef
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-05 22:30:53 +0200

    new fun loadUsersFromJson(), new fun register(), new fun printLog()added

--- websiteExPhp/common.php
+++ websiteExPhp/common.php
@@ -13,22 +13,82 @@ function loadUsers():array {
   }
 }
 
-
-// controlla se l user esiste ritornando true
-function login($username,$password):bool
+function loadUsersFromJson():array
 {
-  $users = loadUsers();
-  foreach ($users as $user) {
-    $currentUser = $user[0];
-    $currentPassword = $user[1];
+  $content = file_get_contents("users.json");
+  $users = json_decode($content, true);
 
-    if($username === $currentUser && $password === $currentPassword) {
-      return true;
-    }
+  if($users === null) {
+    echo "Impossibile caricare gli utenti", $users;
   }
-  return false;
+  return $users;
 }
 
+// controlla se l user esiste ritornando true
+/*
+- restituisce una stringa di errore se il login non e` andato a buon fine
+* restituisce true se il login e` andato a buon fine
+*/
+function login(string $username, string $password)
+{
+  // users recuperati dal file users.csv
+  // $users = loadUsers();
+
+  // users recuperati dal file users.csv
+  $users = loadUsersFromJson();
+  // if($users === $usersFromJson) {
+  //   var_dump(true);die;
+  // }
+
+  foreach ($users as $user) {
+    $currentUser = $user["username"];
+    $currentPassword = $user["password"];
+
+    if($username === $currentUser) {
+        if($password === $currentPassword) {
+        // imposto una variabile di sessione
+        $_SESSION["username"] = $username;
+
+        // scrivo un log
+        printLog(sprintf("l`utente %s ha effettuato il login", $username));
+        return true;
+      }
+        // potrei non mettere il return, e gestire due errori nel prossimo return, fuori
+        return sprintf("La password non e` corretta, riprova!");
+    }
+  }
+  return sprintf("L` Utente %s non e` stato trovato" , $username);
+}
+
+
+function register(string $username, string $password)
+{
+  $users = loadUsersFromJson();
+
+  var_dump($users);
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+
+  $users[] = [
+    "username" => $username,
+    "password" => $password
+  ];
+
+  file_get_contents("users.json", json_encode($users));
+}
+
+
+function printLog($message) {
+  $message = $message . " \n";
+
+  $logFile = fopen("logs.txt", "a+");
+
+  fwrite($logFile, $message);
+
+  fclose($logFile);
+
+}
 
 /**
 @bool
