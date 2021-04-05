3f3fc39791132261ef489919c75d1e1f25b06211
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-05 22:34:45 +0200

    require_once(bootstrap_css_link.php)  imported,new condiction on line 27

--- websiteExPhp/login.php
+++ websiteExPhp/login.php
@@ -10,35 +10,46 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
     $loginSuccessful = login($username, $password);
 
     if($loginSuccessful === true) {
-      $_SESSION["username"] = $username;
+        header("location: homepage.php");
     }
 }
 ?>
 
 
 <html>
+      <?php require_once('bootstrap_css_link.php')?>
     <body>
-        <?php require_once 'menu.php';?>
+        <?php require_once 'navbar.php';?>
 
         <div class="container">
-
-          <form action="login.php" method="POST">
-              <?php if (userIsAuthenticated() === true):?>
-                <p>Sei gia loggato!</p>
+          <div class="row">
+            <div class="col-md-12">
+              <?php if ($loginSuccessful !== true): ?>
+                <p style="color: red">
+                  <?php echo $loginSuccessful ?>
+                </p>
               <?php endif; ?>
 
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Username</label>
-              <input type="username" name="username" class="form-control" id="exampleInputUsername">
-            </div>
+              <form action="login.php" method="POST">
+                  <?php if (userIsAuthenticated() === true):?>
+                    <p>Sei loggato!</p>
+                  <?php endif; ?>
 
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-            </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Username</label>
+                  <input type="username" name="username" class="form-control" id="exampleInputUsername">
+                </div>
+
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
         </div>
 
     </body>
