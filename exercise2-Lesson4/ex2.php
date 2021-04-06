b3c22538e2e007d36f684ae0c5ea4e2d1161f1f6
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-06 21:04:49 +0200

    first-commit

---
+++ exercise2-Lesson4/ex2.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * array che contiene misurazioni di temperature (scala Farenight)
+ */
+$temperatures = [78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73];
+
+/*
+ * 1) scrivere una funzione averageTemperature($values) che restituisca la media
+  delle misurazioni contenute in $temperatures
+ *
+ * 2) scrivere una funzione maxTemperature($values) che restituisca il valore massimo di $temperatures
+ * 3) scrivere una funzione minTemperature($values) che restituisca il valore minimo di $temperatures
+ * 4) scrivere una funzione sortTemperatures($values) che restituisca un array
+ con i valori di temperatura ordinati
+ * 5) aggiungere a sortTemperatures($values) un secondo parametro per richieder i valori
+  ordinati in senso crescente o decrescente
+ */
+
+
+function averageTemperature($array) {
+    $arr = [];
+    $average = $arr
+    foreach ($array as $value) {
+      $arr[] = $value;
+
+    }
+}
