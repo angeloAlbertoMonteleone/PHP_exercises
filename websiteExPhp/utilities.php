6694967da690d65775d11f94f695dbf594b2410e
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-19 22:25:02 +0200

    new absolute path created in $absolutePath var (/websiteExPhp/logs.txt")

--- websiteExPhp/utilities.php
+++ websiteExPhp/utilities.php
@@ -4,8 +4,10 @@
 /* funzione che aggiunge ad ogni login e logout dell utente, un messaggio nel file logs.txt(database accessi)*/
 function printLog($message) {
   $message = $message . " \n";
+  $path = $_SERVER["DOCUMENT_ROOT"];
+  $absolutePath = sprintf("%s/websiteExPhp/logs.txt", $path);
 
-  $logFile = fopen("logs.txt", "a+");
+  $logFile = fopen($absolutePath, "a+");
 
   fwrite($logFile, $message);
 
