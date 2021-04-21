84b34f3ff5f3435967d3959568a59c880833f182
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-21 23:05:24 +0200

    new html container has been added with php foreach: title, content, date

--- websiteExPhp/public/homepage.php
+++ websiteExPhp/public/homepage.php
@@ -4,6 +4,24 @@
     <?php require_once('../bootstrap_css_link.php')?>
     <body>
         <?php require_once '../navbar.php';?>
+
+        <div class="container">
+          <p>HOME</p>
+            <?php foreach($blogManager->getPost() as $post): ?>
+              <div class="row">
+                <div class="col-md-12">
+                  Title
+                </div>
+                <div class="col-md-12">
+                  Content
+                </div>
+                <div class="col-md-12">
+                  Date
+                </div>
+              </div>
+            <?php endforeach; ?>
+        </div>
+
     </body>
-    <p>HOME</p>
+
 </html>
