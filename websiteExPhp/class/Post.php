44aa57e26c9b975cb63a2c7013589c62f7db329e
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-04-22 22:41:55 +0200

    new datetime param in construct,new setAutUsername fun have been added

--- websiteExPhp/class/Post.php
+++ websiteExPhp/class/Post.php
@@ -8,11 +8,13 @@ class Post
   private $title;
   private $content;
   private $authorUsername;
+  private $creationDate;
 
-  public function __construct($title, $content)
+  public function __construct($title, $content, DateTime $creationDate)
   {
     $this->title = $title;
     $this->content = $content;
+    $this->creationDate = $creationDate;
   }
 
   public function getTitle(){
@@ -25,5 +27,12 @@ class Post
 
   public function getAuthorUsername() {
     return $this->authorUsername;
+  }
+  public function setAuthorUsername($username) {
+    $this->authorUsername = $username;
+  }
+
+  public function getCreationDate() {
+    return $this->creationDate;
   }
 }
