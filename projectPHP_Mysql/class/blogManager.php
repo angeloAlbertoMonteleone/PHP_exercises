cb3b60b81e8d11c132a687202d21f9beed40feaa
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-05-10 21:10:10 +0200

    new getPostsByAuthor fun created

--- projectPHP_Mysql/class/blogManager.php
+++ projectPHP_Mysql/class/blogManager.php
@@ -1,11 +1,18 @@
 <?php
 
 require_once "Post.php";
+require_once "DatabaseManager.php";
 /**
  *
  */
 class blogManager
 {
+
+private $databaseManager;
+
+function __construct() {
+    $this->databaseManager = new DatabaseManager();
+}
 
   /**
   *@return array
@@ -15,6 +22,7 @@ class blogManager
   {
     $posts = $this->loadPosts();
     return array_reverse($posts);
+
   }
 
 
@@ -26,19 +34,29 @@ class blogManager
   public function addPost($title, $content, user $user = null): Post
   {
     // istanzia un nuovo oggetto post
+
+    $posts = $this->getPost();
+
+    $creationDate = (new DateTime('now'))->format('Y-m-d H:i:s');
     $post = new Post(
       $title,
       $content,
-      new DateTime('now'));
+      new DateTime('now')
+      );
 
-    // $username = $user->getUsername();
     // se avevessi avuto un og user avrei fatto, $post->setAuthorUsername($user->getUsername)
     if($user !== null) {
       $post->setAuthorUsername($user->getUsername());
     }
+    $postTitle = $post->getTitle();
+    $postContent = $post->getContent();
+    $userId = $user->getId();
 
     // insert into database del post - query
-    
+    $query = "INSERT INTO post (title, content, publication_date, users_id)
+    VALUES ('$postTitle', '$postContent', '$creationDate', $userId)";
+
+    $executionQuery = $this->databaseManager->executeQuery($query);
     return $post;
   }
 
@@ -52,38 +70,57 @@ class blogManager
   */
   public function loadPosts():array
   {
-    $postPathAbsolute = $this->getPostsPathAbsolute();
+    // $postPathAbsolute = $this->getPostsPathAbsolute();
+    //
+    // if(!file_exists($postPathAbsolute)){
+    //   return [];
+    // }
+    //
+    // $content = file_get_contents($postPathAbsolute);
 
-    if(!file_exists($postPathAbsolute)){
-      return [];
+    // if($posts === null) {
+    //   echo "Impossibile caricare i posts ", json_last_error_msg();
+    // }
+
+
+    $query = "SELECT * FROM post";
+
+    try {
+      $statement = $connection->query($query);
+    } catch (\Exception $e) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      die();
     }
 
-    $content = file_get_contents($postPathAbsolute);
-    $posts = json_decode($content, true);
+    $posts = $statement->fetchAll();
 
-    if($posts === null) {
-      echo "Impossibile caricare i posts ", json_last_error_msg();
+
+      // if(array_key_exists("author_username",$post)) {
+      //   $postsObjects->setAuthorUsername($post["author_username"]);
+      // }
+      //
+      // $posts[$key] = $postsObjects;
+      return $postsObjects;
     }
 
-  // costruisce un array di oggetti post
-    foreach ($posts as $key => $post) {
-      $creationDate = DateTime::createFromFormat("Y-m-d H:i:s", $post["creation_date"]);
-      $postsObjects = new Post(
+
+
+  private function buildPostbySelectedResults(array $records) {
+
+    $posts = [];
+    // costruisce un array di oggetti post
+    foreach ($records as $key => $post) {
+      $creationDate = DateTime::createFromFormat("Y-m-d H:i:s", $post["publication_date"]);
+      $posts[] = new Post(
         $post["title"],
         $post["content"],
         $creationDate
       );
-
-      if(array_key_exists("author_username",$post)) {
-        $postsObjects->setAuthorUsername($post["author_username"]);
-      }
-
-      $posts[$key] = $postsObjects;
     }
 
-    return $posts;
-  }
 
+      return $posts;
+  }
 
 
   /**
@@ -109,6 +146,27 @@ class blogManager
     file_put_contents($postPathAbsolute, json_encode($output));
   }
 
+
+
+  public function getPostsByAuthor(string $username) {
+
+    $query = "SELECT Id FROM people WHERE username = '$username'";
+    // var_dump($query);die;
+
+    $idsArray = $this->databaseManager->executeQuery($query);
+
+    if(count($idsArray) > 0) {
+      $userId = $ids[0]['Id'];
+    } else {
+      // se l utente non ha scritto post ritorniamo un array vuoto
+      return [];
+    }
+
+    $queryPosts = "SELECT * FROM post WHERE users_id = $userId ORDER BY publication_date DESC";
+
+    $postsArray = $this->databaseManager->executeQuery($queryPosts);
+
+  }
 
 
 
