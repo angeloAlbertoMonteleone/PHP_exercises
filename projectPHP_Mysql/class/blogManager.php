524e2c5593c116527a8510af004249adb804d47f
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-05-12 22:51:19 +0200

    findPost,deletePost functions created, parametr queries have been set up

--- projectPHP_Mysql/class/blogManager.php
+++ projectPHP_Mysql/class/blogManager.php
@@ -14,6 +14,9 @@ function __construct() {
     $this->databaseManager = new DatabaseManager();
 }
 
+
+
+
   /**
   *@return array
   * file che ristituisce l`array dei posts
@@ -22,8 +25,30 @@ function __construct() {
   {
     $posts = $this->loadPosts();
     return array_reverse($posts);
-
   }
+
+
+
+  /**
+  *@return array
+  * file che ristituisce l`array dei posts tramite l`id
+  */
+  public function findPost(int $id): Post
+  {
+    $queryPost = "SELECT * FROM usersdb.post WHERE id = :id";
+
+    $results = $this->databaseManager->executeQuery($queryPost,
+    ['id' => $id]);
+
+    if(count($results) < 1) {
+      return null;
+    }
+
+    $firstRow = $results[0];
+
+    return $this->buildPostbySingleResult($firstRow);
+  }
+
 
 
 
@@ -35,14 +60,12 @@ function __construct() {
   {
     // istanzia un nuovo oggetto post
 
-    $posts = $this->getPost();
-
-    $creationDate = (new DateTime('now'))->format('Y-m-d H:i:s');
     $post = new Post(
       $title,
       $content,
       new DateTime('now')
       );
+
 
     // se avevessi avuto un og user avrei fatto, $post->setAuthorUsername($user->getUsername)
     if($user !== null) {
@@ -51,12 +74,17 @@ function __construct() {
     $postTitle = $post->getTitle();
     $postContent = $post->getContent();
     $userId = $user->getId();
+    $creationDate = (new DateTime('now'))->format('Y-m-d H:i:s');
 
     // insert into database del post - query
-    $query = "INSERT INTO post (title, content, publication_date, users_id)
-    VALUES ('$postTitle', '$postContent', '$creationDate', $userId)";
+    $query = "INSERT INTO usersdb.post (title, content, publication_date, users_id)
+    VALUES (:postTitle, :postContent, :creationDate, :userId)";
 
-    $executionQuery = $this->databaseManager->executeQuery($query);
+    $executionQuery = $this->databaseManager->executeQuery($query,
+  [ 'postTitle' => $postTitle,
+    'postContent' => $postContent,
+    'creationDate' => $creationDate,
+    'userId'=> $userId]);
     return $post;
   }
 
@@ -66,24 +94,13 @@ function __construct() {
   /**
   *@return array
   *@throws Exception
-  * estrapola i posts dal file posts.json e ritorna dei posts in un array di oggetti(decodificati in arrays)
+  * estrapola i posts dal database e ritorna dei posts in un array di oggetti
   */
   public function loadPosts():array
   {
-    // $postPathAbsolute = $this->getPostsPathAbsolute();
-    //
-    // if(!file_exists($postPathAbsolute)){
-    //   return [];
-    // }
-    //
-    // $content = file_get_contents($postPathAbsolute);
+    $connection = $this->databaseManager->getConnection();
 
-    // if($posts === null) {
-    //   echo "Impossibile caricare i posts ", json_last_error_msg();
-    // }
-
-
-    $query = "SELECT * FROM post";
+    $query = "SELECT * FROM usersdb.post";
 
     try {
       $statement = $connection->query($query);
@@ -93,85 +110,173 @@ function __construct() {
     }
 
     $posts = $statement->fetchAll();
-
-
-      // if(array_key_exists("author_username",$post)) {
-      //   $postsObjects->setAuthorUsername($post["author_username"]);
-      // }
-      //
-      // $posts[$key] = $postsObjects;
-      return $postsObjects;
-    }
-
-
-
-  private function buildPostbySelectedResults(array $records) {
-
-    $posts = [];
-    // costruisce un array di oggetti post
-    foreach ($records as $key => $post) {
+    $output = [];
+    foreach ($posts as $key => $post) {
       $creationDate = DateTime::createFromFormat("Y-m-d H:i:s", $post["publication_date"]);
-      $posts[] = new Post(
+      $output[] = new Post(
         $post["title"],
         $post["content"],
         $creationDate
       );
     }
 
+    return $output;
+}
+
+
+
+
+
+  /**
+  *@param user
+  *@param post
+  * elimina un post
+  */
+  public function deletePost(user $user,Post $post)
+  {
+    $userId = $user->getId();
+    $postId = $post->getId();
+
+    $queryPostId = "SELECT * FROM usersdb.post WHERE id = :postId";
+    $result = $this->databaseManager->executeQuery($queryPostId,
+    ['postId' => $postId]);
+
+    if((int)$userId === (int)$result[0]["users_id"]) {
+      $this->deletePostafterCondiction($postId);
+    } else {
+      return null;
+    }
+
+  }
+
+
+  public function deletePostafterCondiction($postId):void
+  {
+    $query = "DELETE FROM usersdb.post WHERE id = ?";
+    $deleteResult = $this->databaseManager->executeQuery($query,
+    [$postId]);
+  }
+
+
+
+
+  /**
+  *@return void
+  * aggiorna il post con un nuovo post
+  */
+  public function updatePost(Post $post, string $title, string $content):void
+  {
+    $postId = $post->getId();
+
+    $query ="UPDATE usersdb.post SET title = :title, content = :content WHERE id = :postId";
+
+    $result = $this->databaseManager->executeQuery($query,
+    [ 'title' => $title,
+      'content' => $content,
+      'postId' => $postId
+      ]);
+    }
+
+
+
+
+
+  public function getPostsByAuthor(string $username):array
+  {
+
+    $query = "SELECT * FROM usersdb.post_and_authors WHERE username = :username ORDER BY publication_date DESC";
+
+    $result = $this->databaseManager->executeQuery($query,
+    [  'username' => $username  ] );
+
+    return $this->buildPostbySelectedResults($result);
+
+    // 1 option
+    // $connection = $this->databaseManager->getConnection();
+    //
+    // $query = "SELECT * FROM usersdb.post_and_authors WHERE username = '$username' ORDER BY publication_date DESC";
+    // $result = $this->databaseManager->executeQuery($query);
+    //
+    // $resultArray = $result->fetchAll();
+    //
+    // return $this->buildPostbySelectedResults($resultArray);
+
+
+
+
+    // 2 option
+    // $query = "SELECT id FROM people WHERE username = '$username'";
+
+    // $result = $this->databaseManager->executeQuery($query);
+
+    // $idsArray = $statement->fetchAll();
+    //
+    // if(count($idsArray) > 0) {
+    //   $userId = $idsArray[0]['id'];
+    // } else {
+    //   // se l utente non ha scritto post ritorniamo un array vuoto
+    //   return [];
+    // }
+
+    // $queryPosts = "SELECT * FROM post WHERE users_id = $userId ORDER BY publication_date DESC";
+
+    // $result = $this->databaseManager->executeQuery($queryPosts);
+
+    // $postArray = $postStatement->fetchAll();
+
+    // return $this->buildPostbySelectedResults($resultArray);
+
+  }
+
+
+
+
+
+  /**
+  *@return array
+  * costruisce un array di oggetti con dei records passati
+  */
+    private function buildPostbySelectedResults(array $records) {
+      $posts = [];
+      // costruisce un array di oggetti post
+      foreach ($records as $key => $result) {
+        $posts[] = $this->buildPostbySingleResult($result);
+      }
 
       return $posts;
   }
 
 
+
+
   /**
-  *@return void
-  * carica sul nostro database il nuovo users [],(users di array di istanze user) su file json
+  *@return Post
+  * elabora l array di posts, e restituisce un singolo post
   */
-  private function updatePosts(array $posts) {
+  private function buildPostbySingleResult(array $records):Post
+  {
+    // costruisce un array di un post
+    foreach ($records as $key => $result) {
+      $creationDate = DateTime::createFromFormat("Y-m-d H:i:s", $records["publication_date"]);
+      $post = new Post(
+        $records["title"],
+        $records["content"],
+        $creationDate
+      );
 
-    $postPathAbsolute = $this->getPostsPathAbsolute();
+      if(array_key_exists("id", $records)) {
+        $post->setId($records["id"]);
+      }
 
-    $output = [];
-
-    foreach ($posts as $post) {
-      $output[] = [
-        "title" => $post->getTitle(),
-        "content" => $post->getContent(),
-        "author_username" => $post->getAuthorUsername(),
-        "creation_date" => $post->getCreationDate()->format("Y-m-d H:i:s")
-      ];
-
-    }
-    // convertiamo users in array, da un array di oggetti
-    file_put_contents($postPathAbsolute, json_encode($output));
   }
-
-
-
-  public function getPostsByAuthor(string $username) {
-
-    $query = "SELECT Id FROM people WHERE username = '$username'";
-    // var_dump($query);die;
-
-    $idsArray = $this->databaseManager->executeQuery($query);
-
-    if(count($idsArray) > 0) {
-      $userId = $ids[0]['Id'];
-    } else {
-      // se l utente non ha scritto post ritorniamo un array vuoto
-      return [];
-    }
-
-    $queryPosts = "SELECT * FROM post WHERE users_id = $userId ORDER BY publication_date DESC";
-
-    $postsArray = $this->databaseManager->executeQuery($queryPosts);
-
+  return $post;
   }
 
 
 
 
-    /*prende la path assoluta del posts.json*/
+
+/*prende la path assoluta del posts.json*/
     public function getPostsPathAbsolute() {
       $root = $_SERVER["DOCUMENT_ROOT"];
       $postPath = sprintf('%s/websiteExPhp/posts.json',$root);
